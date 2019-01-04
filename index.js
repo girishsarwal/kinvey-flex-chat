391edbd86ce39dd5471ecb9bc04f12482b7a39af
Girish Sarwal <g.sarwal@gmail.com>
2019-01-04 19:53:45 +0530

    updated comments and added sample payloads for reference

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ sdk.service((err, flex) => {
                     return complete().runtimeError("cannot find message to update");
                 }
                 let res = found.content.to.map(function(a){
+                    /** for each recipient, the read status needs to be the 
+                     * existing read status || the new status that has been passed in the request 
+                     * **/
                     a.read = a.read || context.body.content.to.find(b => b.user == a.user).read;
                     return a;
                 });
@@ -39,3 +42,59 @@ sdk.service((err, flex) => {
     });
     
 });
+
+
+/** Sample paytloads
+ * */
+
+/** 
+ * New Chat Message
+ * 
+ * POST /appdata/kid_HkasmodbE/messages
+ * 
+ * {
+  "content": {
+    "subject": "HNY!",
+    "body": "Wish you a very Happy new year 2019!",
+    "from": "5c2b289b9ff0ad454ed1bb2b",
+    "to": [
+      {
+        "user": "5c2b28ac32909717a6cbb419",
+        "read": false,
+        "read_at": null
+      },
+      {
+        "user": "5c2b28a44c560211479439b3",
+        "read": false,
+        "read_at": null
+      }
+    ]
+  }
+}
+*/
+
+/** Existing Chat Message (recipient marks read) 
+ * Recipient only changes his read flag to true
+ * 
+ * PUT /appdata/kid_HkasmodbE/messages/{messageId}
+ * 
+ * {
+  "content": {
+    "subject": "HNY!",
+    "body": "Wish you a very Happy new year 2019!",
+    "from": "5c2b289b9ff0ad454ed1bb2b",
+    "to": [
+      {
+        "user": "5c2b28ac32909717a6cbb419",
+        "read": true,
+        "read_at": null
+      },
+      {
+        "user": "5c2b28a44c560211479439b3",
+        "read": false,
+        "read_at": null
+      }
+    ]
+  }
+}
+*/
